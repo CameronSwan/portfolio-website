16ec4153a88b29ddef5261ddbeded764c756188f
Cameron Swan <swanc@dal.ca>
2023-01-07 16:42:25 -0400

    Routeing Setup. app.js Configured.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const userRouter = require('./users')
+const projectRouter = require('./projects')
 
-module.exports = router;
+router.use('/users', userRouter)
+router.use('/projects', projectRouter)
+
+module.exports = router
